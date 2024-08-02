9b9206e7d02826d3486013e04735c064831eff2f
Vasilij Moiseev <vasilij.june@gmail.com>
2024-08-02 23:12:11 +0300

    java-oop-1 | homework is done
    java-oop-1 | homework is done

---
+++ src/main/java/ru/otus/java/basic/homeworks/lesson9/User.java
@@ -0,0 +1,44 @@
+package ru.otus.java.basic.homeworks.lesson9;
+
+public class User {
+    private String name;
+    private String surname;
+    private String middleName;
+    private int yearOfBirth;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User(String name, String surname, String middleName, int yearOfBirth, String email) {
+        this.name = name;
+        this.surname = surname;
+        this.middleName = middleName;
+        this.yearOfBirth = yearOfBirth;
+        this.email = email;
+    }
+
+    public String getInfo() {
+        return String.format("""
+                ФИО: %s %s %s
+                Год рождения: %d
+                e-mail: %s""", surname, name, middleName, yearOfBirth, email);
+    }
+}
